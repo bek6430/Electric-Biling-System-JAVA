4f43c49006dc527a19f093e889132195ed23ce74
Bereket Birhanu <99336739+bek6430@users.noreply.github.com>
2023-06-24 09:05:58 +0300

    Add files via upload

---
+++ java/com/servlet/AdminProfile.java
@@ -0,0 +1,47 @@
+package com.servlet;
+
+import com.servlet.registration.Customer;
+import controllers.DBcontroller;
+import java.io.IOException;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class AdminProfile extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        Customer customer = (Customer) req.getSession().getAttribute("user");
+        if (customer != null) {
+            RequestDispatcher view = req.getRequestDispatcher("adminProfile.jsp");
+            view.forward(req, resp);
+        } else {
+            resp.sendRedirect("index.jsp");
+        }
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        Customer customer = (Customer) req.getSession().getAttribute("user");
+        if (customer != null) {
+            try {
+                PreparedStatement preparedStatementAll = DBcontroller.getCon().prepareStatement("SELECT * FROM customer WHERE userId = '40'");
+                ResultSet rsAll = preparedStatementAll.executeQuery();
+                req.setAttribute("resultSet", rsAll);
+                RequestDispatcher view = req.getRequestDispatcher("adminProfile.jsp");
+                view.forward(req, resp);
+            } catch (SQLException ex) {
+                Logger.getLogger(Login.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        } else {
+            resp.sendRedirect("index.jsp");
+        }
+    }
+}
